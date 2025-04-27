a8c8df17d30cfc89518a2ca5ac82d9eb2c67f057
Ahnaf Sindid <sindma@farmingdale.edu>
2025-04-27 11:58:29 -0400

    customer Menu Screen UI with buttons and css

---
+++ src/main/java/murray/csc325sprint1/CustomerMenuController.java
@@ -0,0 +1,27 @@
+package murray.csc325sprint1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CustomerMenuController {
+
+    @FXML
+    private Button cusContact;
+
+    @FXML
+    private Button cusLogOut;
+
+    @FXML
+    private Button cusMenu;
+
+    @FXML
+    private Button cusOrder;
+
+    @FXML
+    private Button cusOrderHistory;
+
+    @FXML
+    private Button cusQuote;
+
+}
+
---
+++ src/main/java/murray/csc325sprint1/CustomerMenuMain.java
@@ -0,0 +1,24 @@
+package murray.csc325sprint1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CustomerMenuMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/murray/csc325sprint1/customer-main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
