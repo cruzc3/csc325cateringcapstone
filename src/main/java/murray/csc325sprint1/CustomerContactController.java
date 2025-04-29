7e9695a6c13d3497318422d2a25b986dbe2c6d02
Ahnaf Sindid <sindma@farmingdale.edu>
2025-04-29 12:45:45 -0400

    INIT COMMIT

---
+++ src/main/java/murray/csc325sprint1/CustomerContactController.java
@@ -0,0 +1,16 @@
+package murray.csc325sprint1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CustomerContactController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
