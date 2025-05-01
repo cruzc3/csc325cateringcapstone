46cf1136e982946d26b87ff439aff3dfe4d21acd
Ahnaf Sindid <sindma@farmingdale.edu>
2025-05-01 13:25:29 -0400

    employee contact fxml and controller(half done)

---
+++ src/main/java/murray/csc325sprint1/empContactController.java
@@ -0,0 +1,12 @@
+package murray.csc325sprint1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class empContactController {
+
+    @FXML
+    private Button empGoBackBtn;
+
+}
+
