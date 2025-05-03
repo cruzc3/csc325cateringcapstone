5e86a6eadf4ca57ddbb8eacddf59a0038df20718
Ahnaf Sindid <sindma@farmingdale.edu>
2025-05-03 11:07:49 -0400

    added Order button functionality in Customer menu screen

--- src/main/java/murray/csc325sprint1/CustomerMenuController.java
+++ src/main/java/murray/csc325sprint1/CustomerMenuController.java
@@ -1,7 +1,15 @@
 package murray.csc325sprint1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
 public class CustomerMenuController {
 
@@ -23,5 +31,17 @@ public class CustomerMenuController {
     @FXML
     private Button cusQuote;
 
+    @FXML
+    private void setCusOrder(ActionEvent event) {
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/murray/csc325sprint1/OrderView.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
 
