7788930a59ee84adb7d5de1a1e90d529579c7931
Ahnaf Sindid <sindma@farmingdale.edu>
2025-05-06 13:09:26 -0400

    INIT COMMIT

---
+++ src/main/java/murray/csc325sprint1/ViewModel/empContactController.java
@@ -0,0 +1,16 @@
+package murray.csc325sprint1.ViewModel;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class empContactController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
