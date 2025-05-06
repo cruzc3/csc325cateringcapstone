f2cba606e5550c4e6e88ff333c983ec35d3be416
Carlos CruzSerrano <ccserrano5@outlook.com>
2025-05-06 13:27:51 -0400

    customer contact ui 3

---
+++ src/main/java/murray/csc325sprint1/ViewModel/customerContactController.java
@@ -0,0 +1,28 @@
+package murray.csc325sprint1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class customerContactController {
+
+    @FXML
+    private TextArea EnterMessage;
+
+    @FXML
+    private TextField EnterSubject;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private Button viewRequest;
+
+    @FXML
+    void handleSendButton(ActionEvent event) {
+
+    }
+
+}
