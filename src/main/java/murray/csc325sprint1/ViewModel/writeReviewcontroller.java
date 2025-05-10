5f69304dfd37937c676f3b6eb778957ec2078089
Ahnaf Sindid <sindma@farmingdale.edu>
2025-05-09 20:41:31 -0400

    added customer write a review fxml and controller

--- src/main/java/murray/csc325sprint1/ViewModel/writeReviewcontroller.java
+++ src/main/java/murray/csc325sprint1/ViewModel/writeReviewcontroller.java
@@ -3,6 +3,6 @@ package murray.csc325sprint1.ViewModel;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class customerReviewController  {
+public class writeReviewcontroller {
 
 }
